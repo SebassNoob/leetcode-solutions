90a6bd72b9a9cbf68cb0be681f7b5c0e674d16aa
SebassNoob <78428559+SebassNoob@users.noreply.github.com>
2023-02-18 00:04:36 +0800

    Time: 72 ms (74.68%), Space: 42.4 MB (75.42%) - LeetHub

---
+++ 0263-ugly-number/0263-ugly-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isUgly = function(n) {
+    if (n === 0){return false;}
+            
+    while (n%5 === 0){n = n/5;}
+        
+    while (n%3 === 0){n = n/3;}
+        
+    while (n%2 === 0){n = n/2;}
+        
+    if (n == 1){return true;} 
+    return false;
+};
